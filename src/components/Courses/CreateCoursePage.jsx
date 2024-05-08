e972c00c71ce7ecdc0d10f1ce959d2642464fb72
CHANNAKESHAVAS <channakeshavas@HMLT0008854.local>
2024-05-08 22:19:09 +0530

    test

--- src/components/Courses/CreateCoursePage.jsx
+++ src/components/Courses/CreateCoursePage.jsx
@@ -46,7 +46,7 @@ function CreateCoursePage({ createCourse }) {
                 </div>
                 <div className="mb-5">
                     <label htmlFor="courseNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course Number</label>
-                    <input type="text" id="courseNumber" name="courseNumber" value={formData.courseNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 101" required />
+                    <input type="number" id="courseNumber" name="courseNumber" value={formData.courseNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 101" required />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
