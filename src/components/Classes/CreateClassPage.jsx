94f3d4271fc66fcf8dc51afcb4219e56d801b203
CHANNAKESHAVAS <channakeshavas@HMLT0008854.local>
2024-05-08 22:08:12 +0530

    test

--- src/components/Classes/CreateClassPage.jsx
+++ src/components/Classes/CreateClassPage.jsx
@@ -56,11 +56,11 @@ function CreateClassPage({ createClass }) {
                 </div>
                 <div className="mb-5">
                     <label htmlFor="courseNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course Number</label>
-                    <input type="text" id="courseNumber" name="courseNumber" value={formData.courseNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 101" required />
+                    <input type="number" id="courseNumber" name="courseNumber" value={formData.courseNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 101" required />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="sectionNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Section Number</label>
-                    <input type="text" id="sectionNumber" name="sectionNumber" value={formData.sectionNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 01" required />
+                    <input type="number" id="sectionNumber" name="sectionNumber" value={formData.sectionNumber} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="e.g., 01" required />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="year" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
